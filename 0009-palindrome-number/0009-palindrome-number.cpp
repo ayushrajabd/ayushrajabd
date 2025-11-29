0edd23d15011bbc3af0fb1b80a574e6967bc8ac8
ayushrajabd <ayushrajabd2021@gmail.com>
2025-11-29 16:35:11 +0530

    Time: 4 ms (32.92%), Space: 8.6 MB (42.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    int isreverse(int x){
+        int result=0;int c=x;
+        while(x!=0){
+            int temp=x%10;
+            if(result<INT_MIN/10||result>INT_MAX/10)return 0;
+            result=result*10+temp;x=x/10;
+        }return result;
+    }
     bool isPalindrome(int x) {
+        int c=isreverse(x);
         if(x<0){return false;}
-
-        int org=x;long long rev=0;int digit;
-        while(org>0){
-            digit=org%10;
-            rev=rev*10+digit;
-            org=org/10;
-        }
-        return (static_cast<int>(rev))==x;
+        if(c==x)return true;
+        else return false;
         
     }
 };
