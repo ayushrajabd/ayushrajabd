62e15cc81d565fc3d2bbb4e8c2e27ba4ed0b211a
ayushrajabd <ayushrajabd2021@gmail.com>
2025-11-29 16:19:35 +0530

    Time: 2 ms (38.83%), Space: 8.4 MB (82.26%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+    bool bol(int x){
+        if(x<0)return true;
+        else return false;
+    }
     int reverse(int x) {
+        long long result=0;
+        if(x==0)return 0;
         
-       long long result=0;
-       while(x!=0){
-        int digit=x%10;
-        result=result*10+digit;
-        x=x/10;
-
-       }
-       if(result<INT_MIN||result>INT_MAX){
-        return 0;
-       }
-       else return static_cast<int>(result);
+        while(x!=0){
+            ;
+            int temp=x%10;
+            if(result>INT_MAX/10||result<INT_MIN/10)return 0;
+            result=(result*10)+temp;
+        
+            x=x/10;
+        }
+        if(0) {return -1*result;}
+        return result;
+       
     }
 };
