a7c72644e0178fc317505047956b3c70cd099f37
ayushrajabd <ayushrajabd2021@gmail.com>
2025-12-02 18:30:43 +0530

    Time: 11 ms (32.77%), Space: 139 MB (20.16%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       vector <int> ans(nums.size());int pos=0;int neg=1;
+       vector<int> ans(nums.size());
+       vector<int> pos;
+       vector<int> neg;
        for(int i=0;i<nums.size();i++){
-        if(nums[i]<0){
-            ans[neg]=nums[i];
-            neg+=2;
+        if(nums[i]>=0){
+            pos.push_back(nums[i]);
         }
         else{
-            ans[pos]=nums[i];
-            pos+=2;
+            neg.push_back(nums[i]);
+        }}
+        if(pos.size()>neg.size()){
+            for(int i=0;i<neg.size();i++){
+                ans[2*i]=pos[i];
+                ans[2*i+1]=neg[i];
+            }
+            int index=2*neg.size();
+            for(int i=neg.size();i<pos.size();i++){
+                ans[index]=pos[i];index++;
+            }
         }
+        else{
+            for(int i=0;i<pos.size();i++){
+                ans[2*i]=pos[i];
+                ans[2*i+1]=neg[i];
+            }
+            int index=2*pos.size();
+            for(int i=pos.size();i<neg.size();i++){
+                ans[index]=neg[i];index++;
+            }
+        
 
-       }
-       return ans;
+       }return ans;
     }
 };
