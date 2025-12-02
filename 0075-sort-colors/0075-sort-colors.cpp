ff86ce96d65485499c48fbe9242bab5e4a8e36a7
ayushrajabd <ayushrajabd2021@gmail.com>
2025-12-02 10:33:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,21 @@
 class Solution {
-public:
+public://dutch flag Algorithm
     void sortColors(vector<int>& nums) {int count0=0,count1=0,count2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)count0++;
-            else if(nums[i]==1)count1++;
-            else count2++;
+        int low=0;int mid=0;int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;mid++;
 
+            }
+            else if(nums[mid]==1){mid++;
 
-        }
-        for(int i=0;i<count0;i++){
-            nums[i]=0;
-        }
-        for(int i=count0;i<count0+count1;i++){
-            nums[i]=1;
-        }
-        for(int i=count0+count1;i<nums.size();i++){
-            nums[i]=2;
+            }
+            else {
+                swap(nums[mid],nums[high]);
+                high--;
+
+            }
         }
     }
 };
