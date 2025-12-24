dae4822bec9d2e612fa4308c5e742034be1a2180
ayushrajabd <ayushrajabd2021@gmail.com>
2025-12-24 07:01:34 +0530

    Time: 0 ms (100%), Space: 17.6 MB (16.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
 
-    int lowerbound(vector<int>& nums,int n,int x){
-        int start=0;int end=n-1;int ans=n;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]>=x){
-                ans=mid;
-                end=mid-1;
-
-            }
-            else{
-                start=mid+1;
-            }
-
-        }
-        return ans;
-    }
-    int upperbound(vector<int>& nums,int n,int x){
-        int start=0;int end=n-1;int ans=n;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]>x){
-                ans=mid;
-                end=mid-1;
-
-            }
-            else{
-                start=mid+1;
-            }
-
-        }
-        return ans;
-    }
     
-    vector<int> searchRange(vector<int>& nums, int target) {
+    
+    vector<int> searchRange(vector<int>& nums, int x) {
         int n=nums.size();
-        int start=lowerbound(nums,n,target);
-        if(start==n||nums[start]!=target)return{-1,-1};
-        int end=upperbound(nums,n,target);
-        return {start,end-1};
+        int low=0;int high=n-1;int fo=-1;int lo=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==x){
+                fo=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+         low=0; high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==x){
+                lo=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return{fo,lo};
     }
 };
