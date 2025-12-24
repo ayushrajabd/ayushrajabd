6b06e29033a4d027573e8be0f1a35820463d6d5c
ayushrajabd <ayushrajabd2021@gmail.com>
2025-12-24 19:23:31 +0530

    Time: 0 ms (100%), Space: 17.9 MB (55.12%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,19 +8,19 @@ public:
 
             if (arr[mid] == target) return true;
 
-            // ⭐ FIX: handle duplicates
+            
             if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
                 low++;
                 high--;
             }
-            // Left half sorted
+            
             else if (arr[low] <= arr[mid]) {
                 if (arr[low] <= target && target < arr[mid])
                     high = mid - 1;
                 else
                     low = mid + 1;
             }
-            // Right half sorted
+            
             else {
                 if (arr[mid] < target && target <= arr[high])
                     low = mid + 1;
