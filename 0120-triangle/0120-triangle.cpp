d6c5347fc31fd8b07ff035a69e7fb6c73e81a77a
ayushrajabd <ayushrajabd2021@gmail.com>
2026-01-20 09:31:15 +0530

    Time: 0 ms (100%), Space: 13.6 MB (8.11%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,19 +13,21 @@ public:
         int n=triangle.size();
         vector<vector<int>> dp
         (n);
+        vector<int> front(n,0),curr(n,0);
         for(int i=0;i<n;i++){
             dp[i]=vector<int>(i+1,-1);
         }
         for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
+            front[j]=triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                int d=triangle[i][j]+dp[i+1][j];
-                int dg=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(d,dg);
+                int d=triangle[i][j]+front[j];
+                int dg=triangle[i][j]+front[j+1];
+                curr[j]=min(d,dg);
             }
+            front=curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
