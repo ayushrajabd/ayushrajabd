1faef09a814631b97f7ef66e9cdff90f8c9aa3cb
ayushrajabd <ayushrajabd2021@gmail.com>
2026-07-17 23:33:24 +0530

    Time: 43 ms (72%), Space: 132 MB (38.67%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> freq(mx + 1, 0);
+        for (int x : nums) freq[x]++;
+
+        vector<long long> cnt(mx + 1, 0);
+
+        
+        for (int d = 1; d <= mx; d++) {
+            for (int m = d; m <= mx; m += d)
+                cnt[d] += freq[m];
+        }
+
+        vector<long long> exact(mx + 1, 0);
+
+       
+        for (int d = mx; d >= 1; d--) {
+            long long pairs = cnt[d] * (cnt[d] - 1) / 2;
+            for (int m = d + d; m <= mx; m += d)
+                pairs -= exact[m];
+            exact[d] = pairs;
+        }
+
+       
+        vector<long long> pref(mx + 1, 0);
+        for (int d = 1; d <= mx; d++)
+            pref[d] = pref[d - 1] + exact[d];
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+
+        for (long long q : queries) {
+            int l = 1, r = mx;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (pref[mid] > q)
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+            ans.push_back(l);
+        }
+
+        return ans;
+    }
+};
